0e73491c29287ad492e55f4c01f0d692c17efb70
Graham <grh.mrg@gmail.com>
2021-06-26 14:55:27 +0200

    first commit

---
+++ callbacks.js
@@ -0,0 +1,28 @@
+const posts = [
+    {title:'Post one', body:'this is post one'},
+    {title:'Post two', body:'this is post two'}
+];
+
+const newPost = {title:'Post three', body:'this is post three'}
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post,index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    },1000)
+}
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    },2000)
+}
+
+createPost(newPost,getPosts);
+
+//setTimeout() para simular comunicacion con servidor
+
