5db521f9da5cf6dc6b8b31fada41f42cbc697e07
Graham <graham.marengo.contractor@bbva.com>
2021-06-28 14:20:09 +0200

    added async-await

--- async-await.js
+++ async-await.js
@@ -29,26 +29,17 @@ function createPost(post) {
     }); 
 }
 
-// createPost(newPost)
-// .then(getPosts)
-// .catch(err => console.log(err));
+async function init() {
+    await createPost(newPost);
+    getPosts();
+}
 
-//PROMISE.ALL
-const promise1 = Promise.resolve('hello world');
-const promise2 = 10;
-const promise3 = new Promise((resolve,reject) => {
-    setTimeout(resolve,2000,'Goodbye')
-});
-const promise4 = createPost(newPost).then(getPosts).catch(err => console.log(err));
-const promise5 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+init();
 
-Promise.all([promise1,promise2,promise3,promise4,promise5])
-.then((values) => {
-     console.log(values);
-})
-//tardara en resolverse la duracion de la promesa que mas tarde en resolverse
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data);
+}
 
-
-
-
-
+fetchUsers();
