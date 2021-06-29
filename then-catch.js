70a1ddc563246e59eb405a932fd4aa7569cdacfb
Graham <graham.marengo.contractor@bbva.com>
2021-06-29 10:27:50 +0200

    correction

--- then-catch.js
+++ then-catch.js
@@ -1,8 +1,8 @@
 import { newPost, getPosts, createPost } from './promises.js'; 
 
-// createPost(newPost)
-// .then(getPosts)
-// .catch(err => console.log(err));
+createPost(newPost)
+.then(getPosts)
+.catch(err => console.log(err));
 
 //PROMISE.ALL
 const promise1 = Promise.resolve('hello world');
@@ -10,10 +10,9 @@ const promise2 = 10;
 const promise3 = new Promise((resolve,reject) => {
     setTimeout(resolve,2000,'Goodbye')
 });
-const promise4 = createPost(newPost).then(getPosts).catch(err => console.log(err));
-const promise5 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
-Promise.all([promise1,promise2,promise3,promise4,promise5])
+Promise.all([promise1,promise2,promise3,promise4])
 .then((values) => {
     console.log('Resolving with then-catch');
     console.log(values);
